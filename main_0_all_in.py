c360c749183c467f0dfa43cd51f492d1108d0870
Yang Zhaonan <yangzhaonan18@qq.com>
2019-05-27 13:48:57 +0800

    add some file made by pycharm
    those files  no not  useful for coding on linux

--- main_0_all_in.py
+++ main_0_all_in.py
@@ -29,9 +29,11 @@ def judge_light(trafficLight):
 
 if __name__ == "__main__":
     trafficLight = cv2.imread('C:\\Users\\qcdz-003\\Pictures\\light\\001.jpg', cv2.IMREAD_COLOR)
+    cv2.imshow("trafficLight", trafficLight)
     start = time.time()
     color, direction, conf = judge_light(trafficLight)  # 最终只使用这一行代码。
     end = time.time()
     print("judge_light()  use time = ", end - start)  # 0.00297
     print("color, direction, conf = ", color, direction, conf)
     print("the end !")
+    cv2.waitKey()
